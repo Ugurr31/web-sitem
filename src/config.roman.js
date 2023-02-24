69f9a8365a11a964fcc8e5d410e865eb577a2301
Ugurr31 <95451043+Ugurr31@users.noreply.github.com>
2023-02-24 07:34:20 +0300

    Update config.roman.js
    Signed-off-by: Ugurr31 <95451043+Ugurr31@users.noreply.github.com>

--- src/config.roman.js
+++ src/config.roman.js
@@ -7,6 +7,7 @@ module.exports = {
         avatarURL: "https://images-ext-1.discordapp.net/external/DBfXoEPtWUFio_QHKsRFyKGVOYA6XL6XUsCKPyp4RcU/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/914146025621577750/5c6ca77276992b6423d01590de576ba0.png"
     },
     website: {
-        description: "Hello! my name is Uğur, I live in Turkey and I am a high school student."
+        description: "Hello! My name is Uğur. I am 18 years old, I live in Turkey and I am a high school student.
+"
     }
 }
