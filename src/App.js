baa9d644e0fa34486e2a65a8edc8f8606928be03
Ugurr31 <95451043+Ugurr31@users.noreply.github.com>
2023-02-24 20:44:39 +0300

    Update App.js
    Signed-off-by: Ugurr31 <95451043+Ugurr31@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
             My Discord address
           </a>
         <a target="_blank" href={`https://www.youtube.com/${youtube}`} className="button-yt">
-          <i className="fa fa-youtube"></i> Youtube
+          <i className="fa fa-youtube"></y> Youtube
         </a>
         <a target="_blank" href={`https://www.instagram.com/${instagram}`} className="button-ig">
           <i className="fa fa-instagram"></i> Instagram
