a01f35cc9fc6ed305f204cc2f48749efae12af3a
Ugurr31 <95451043+Ugurr31@users.noreply.github.com>
2023-03-03 16:41:37 +0300

    Update App.js
    Signed-off-by: Ugurr31 <95451043+Ugurr31@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ const App = () => {
         </i>
       </a>
      </div>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6401f82331ebfa0fe7f07a6d/1gqjr2jud';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
       <div className="Logo">
         <img className="App-logo" src={discord.avatarURL} alt="logo"/>
       </div>
@@ -31,5 +46,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App;
